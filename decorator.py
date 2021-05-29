435f59a16cbbca57a60a0337702ee25a9e4cc6fb
Mateusz walczyk <mateusz.walczyk@vp.pl>
2021-05-29 13:00:06 +0200

    examples for functions decorators

--- decorator.py
+++ decorator.py
@@ -1,18 +1,34 @@
+def html_tag(tag="h1"):
+    def wrapper(fn):
+        def inner(name):
+            result = f"<p>{fn(name)}</p>"
+            result = result.replace(name, f"<{tag}>{name}</tag>")
+
+            return result
+
+        return inner
+
+    return wrapper
+
+
 def capitalize(fn):
     def inner(name):
-        result = fn(name)
-        result = result.replace(name, f"Mr. {name}")
-
-        return result
+        return fn(name.title())
 
     return inner
+
+
 @capitalize
+@html_tag('h2')
 def hello(name):
     return f"Hello {name}"
 
+
 @capitalize
+@html_tag('span')
 def goodbye(name):
     return f"Bye {name}"
 
+print(hello("mateusz"))
 print(hello("mateusz"))
 print(goodbye("mateusz"))
